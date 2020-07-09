2766633224949fb38fbbfad3285e6773c0c1d437
Jonathan Kang <jon.m.kang@gmail.com>
2020-07-09 17:04:24 -0400

    added jwt auth

--- test/reviews-endpoints.spec.js
+++ test/reviews-endpoints.spec.js
@@ -34,18 +34,10 @@ describe('Reviews Endpoints', function() {
       )
     )
 
-    it(`responds 401 'Unauthorized request' when invalid password`, () => {
-      const UserInvalidPass = { user_name: testUsers[0].user_name, password: '' }
-      return supertest(app)
-        .post('/api/reviews')
-        .set('Authorization', helpers.makeAuthHeader(UserInvalidPass))
-        .expect(401, { error: `Unauthorized request` })
-    })
-
     it(`creates an review, responding with 201 and the new review`, function() {
       this.retries(3)
       const testThing = testThings[0]
-      const testUser = testUsers[0]
+      const testUser = testUsers[1]
       const newReview = {
         text: 'Test new review',
         rating: 3,
@@ -53,7 +45,7 @@ describe('Reviews Endpoints', function() {
       }
       return supertest(app)
         .post('/api/reviews')
-        .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+        .set('Authorization', helpers.makeAuthHeader(testUsers[1]))
         .send(newReview)
         .expect(201)
         .expect(res => {
@@ -77,7 +69,7 @@ describe('Reviews Endpoints', function() {
               expect(row.text).to.eql(newReview.text)
               expect(row.rating).to.eql(newReview.rating)
               expect(row.thing_id).to.eql(newReview.thing_id)
-              expect(row.user_id).to.eql(testUser.user_id)
+              expect(row.user_id).to.eql(testUser.id)
               const expectedDate = new Date().toLocaleString()
               const actualDate = new Date(row.date_created).toLocaleString()
               expect(actualDate).to.eql(expectedDate)
